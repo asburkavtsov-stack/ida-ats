d770ecd22ccc3de9674d56e39ac27a0634013053
Artem <a.s.burkavtsov@gmail.com>
2026-04-27 16:56:24 +0300

    fix bug v2

--- candidates/urls.py
+++ candidates/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CandidateViewSet, VacancyViewSet, OrganizationViewSet, UserListView, current_user, CandidateExportCSVView
+from .views import (
+    CandidateViewSet, VacancyViewSet, OrganizationViewSet,
+    UserListView, current_user, CandidateExportCSVView
+)
 
 router = DefaultRouter()
 router.register(r'candidates', CandidateViewSet, basename='candidate')
@@ -9,11 +12,13 @@ router.register(r'organizations', OrganizationViewSet, basename='organization')
 router.register(r'users', UserListView, basename='users')
 
 urlpatterns = [
+    # ЕКСПОРТ ПЕРЕД router — щоб не конфліктував з candidates/{pk}/
+    path('candidates/export/', CandidateExportCSVView.as_view(), name='candidates-export'),
+
     path('', include(router.urls)),
     path('me/', current_user),
     path('users-detail/<int:pk>/', UserListView.as_view({
         'patch': 'partial_update',
         'delete': 'destroy',
     })),
-    path('candidates/export/', CandidateExportCSVView.as_view(), name='candidates-export'),
 ]
