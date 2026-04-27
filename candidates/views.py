6a5b44a7f7cdba3dc0e3eb2cd86e9e8a5a82f6ac
Artem <a.s.burkavtsov@gmail.com>
2026-04-27 16:49:47 +0300

    fix error

--- candidates/views.py
+++ candidates/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView  # ← ДОДАТИ ЦЕ
 from django.contrib.auth.models import User
-from django.db import models  # Додано для Q
+from django.db import models
 import csv
 from django.http import HttpResponse
 from .models import Candidate, Vacancy, UserProfile, Organization
